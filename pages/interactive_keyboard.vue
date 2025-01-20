b083943340e7814c6a7ccc06cee80ddfb7d7c2a6
clay-bugg <kalebutler2525@gmail.com>
2025-01-20 05:59:02 +0000

    added background

--- pages/interactive_keyboard.vue
+++ pages/interactive_keyboard.vue
@@ -13,10 +13,14 @@
   align-items: center;
   height: 100vh;
   gap: 2em;
+  background-image: url('public/images/background_images/notation.webp');
+  background-size: auto;
 }
 
 h1 {
   margin: 0;
+  margin-top: 2em;
   padding: 0;
+  font-size: 2em;
 }
 </style>
