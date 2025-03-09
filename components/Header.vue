2aecb5f8fbd5d24e4754aa33d67293d361455d83
clay-bugg <kalebutler2525@gmail.com>
2025-03-09 02:19:55 +0000

    added nav links

---
+++ components/Header.vue
@@ -0,0 +1,79 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/instruments"><button :class="{ active: route.path === '/instruments' }">Instruments</button></NuxtLink></li>
+        <li><NuxtLink to="/tools"><button :class="{ active: route.path === '/tools' }">Tools</button></NuxtLink></li>
+      </ul>
+    </nav>
+    <NuxtLink to="/"><div class="heading"></div></NuxtLink>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/practice"><button :class="{ active: route.path === '/practice' }">Practice</button></NuxtLink></li>
+        <li><NuxtLink to="/theory"><button :class="{ active: route.path === '/theory' }">Theory</button></NuxtLink></li>
+      </ul> 
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useRoute } from '#imports';
+
+const route = useRoute();
+
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+  height: 100px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  border-bottom: 1px solid black;
+  position: relative;
+}
+
+header::after {
+  content: '';
+  position: absolute;
+  top: 50%;
+  left: 0;
+  width: 100%;
+  background-color: rgb(0,0,0);
+  height: 10px;
+  z-index: 0;
+}
+
+.heading {
+  width: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: url('/images/Header.png');
+  background-position: center;
+  background-size: cover;
+  height:100px;
+  position: relative;
+  z-index: 1;
+}
+.heading:hover {
+  cursor: pointer;
+}
+
+
+h1 {
+  font-size: 3rem;
+  color: black;
+  margin: 0;
+}
+
+nav ul {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: fit-content;
+  list-style: none
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Header />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+</script>
+
+<style scoped>
+
+</style>
