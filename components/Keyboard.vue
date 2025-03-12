6e93aa84a7c6468f4d64bab089825357a22d31e2
clay-bugg <kalebutler2525@gmail.com>
2025-03-12 12:17:43 +0000

    added buttons for note labels

--- components/Keyboard.vue
+++ components/Keyboard.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="keyboard">
+
+    <div class="volume-container">
+      <label for="volume-slider">Vol</label>
+      <input type="range" id="volume-slider" />
+    </div>
+
     <div class="keyboard-inner">
+
       <div class="controls">
-        <div class="note-labels-selector"></div>
-        <div class="arpeggiator"></div>
-        <div class="volume-tone"></div>
+
+        <div class="control note-labels-selector">
+          <div v-for="(noteButton, index) in noteButtons" :key="noteButton" class="note-button-container">
+            <NoteButton />
+          </div>
+        </div>
+
+        <div class="control arpeggiator">
+          <div class="arpeggiator-switch-container">
+            <label for="arpeggiator">Arppeggiator</label>
+            <input v-model="arpeggiated" type="checkbox" />
+          </div>
+          <input type="number" />
+        </div>
+
+        <div class="control volume-tone">
+
+        </div>
+
       </div>
+
       <div class="keys"></div>
+
     </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+
+const noteButtons = ref([
+  'All', 'Chord', 'None'
+])
+
+const arpeggiated = ref(false);
+
 </script>
 
 <style scoped>
@@ -27,6 +59,13 @@ import { ref } from 'vue'
   flex-direction: column;
   padding: 0 2em 1em;
 }
+
+.volume-container {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+}
 .keyboard-inner {
   background-color: rgb(52, 52, 52);
   width: 100%;
@@ -34,5 +73,46 @@ import { ref } from 'vue'
   border-radius: 35px;
   border-bottom-left-radius: 10px; border-bottom-right-radius: 10px;
   border: 1px solid black;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 30% 70%;
 }
+
+.controls {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 2em;
+  border-bottom: 10px solid black;
+}
+.control {
+  width: 160px;
+}
+
+.note-labels-selector {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  justify-content: space-evenly;
+  gap: 0.2em;
+}
+
+.note-button-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  gap: 0.2em;
+}
+
+.arpeggiator {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  height: fit-content;
+  gap: 0.5em;
+}
+
+
 </style>
---
+++ components/NoteButton.vue
@@ -0,0 +1,49 @@
+<template>
+  <label></label>
+  <button :class="{ on: isOn }"  @click="toggleButton">
+    <div class="inner-button">
+
+    </div>
+  </button>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const isOn = false;
+
+function toggleButton() { 
+  props.isOn = !props.isOn;
+}
+</script>
+
+<style scoped>
+button {
+  width: 50px;
+  height: 50px;
+  background-color: rgb(255, 0, 0);
+  border-radius: 50px;
+  border: 5px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  appearance: none;
+  padding: 0;
+  }
+button:hover {
+  cursor: pointer;
+  transform: scale(1.04) 
+}
+button:active {
+  transform: scale(1)
+}
+.button-inner {
+  height: 30px;
+  width: 30px;
+  background-color: grey;
+}
+.on {
+    
+}
+</style>
